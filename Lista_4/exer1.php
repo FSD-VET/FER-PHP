90b005158384cdbf3a45a4da4008bb826e614c66
FSD-VET <fsd.vet@gmail.com>
2025-03-06 21:22:49 -0300

    Lista 4

---
+++ Lista_4/exer1.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 4 - Exercício 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+ 
+    <h1><center>Lista 4 - Exercício 1<center></h1><br>
+
+
+    <h1><center>Lendo uma Palavra e apresentando o número de Caracteres<center></h1><br>
+
+
+    <form method="post" action = "exer1resposta.php">
+        
+    <main class="container mt-3">
+        <form  method="post" action="exer1resposta.php">
+    
+                        
+    <div class="row mb-3">
+            <label for="palavra" class="form-label">Informe uma palavra</label>
+            <input type="text" id="palavra" name="palavra" class="form-control" required><br><br>
+        </div>
+                    
+                         
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>       
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Lista_4/exer2resposta.php
@@ -0,0 +1,45 @@
+<?php
+    declare(strict_types=1); //obriga que todas as funções e variaveis tenham algum tipo definido
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 4 - Exercício 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Lista 4 - Exercício 2</h1><br>
+
+    <?php
+
+        function manipularString(string $palavra) : void {
+            echo "<p>Maiúsculas: " . strtoupper($palavra) . "</p>";
+            echo "<p>Minúsculas: " . strtolower($palavra) . "</p>";
+            
+           
+        }
+
+       
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            try { //verificando as informações do formulario - enviadas
+                $palavra = $_POST['palavra'];
+                manipularString($palavra);
+  
+            } 
+    
+            catch(Exception $e) {  //ele armazema o erro - se existir  //$e é o pedacinho de memória que armazena o erro
+                echo $e->getMessage(); //exibe o erro
+            }
+               
+           
+        }
+                        
+    ?>
+      
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
